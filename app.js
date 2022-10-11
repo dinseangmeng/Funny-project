5420d1a8a7eb77a096e91284175e8fc06620335b
dinseangmeng <dinseangmeng@gmail.com>
2022-10-11 08:26:41 +0700

    Addd feature

--- app.js
+++ app.js
@@ -4,37 +4,46 @@ function prank(){
     var contianer=document.getElementsByClassName("container")[0];
     btn.addEventListener("click",(e)=>{
         i++;
-        const ele=e.target;
-        const ele0=document.getElementsByClassName("btn")[0].getBoundingClientRect();
-        if(ele.classList.contains("no")){
-            var x,y;
-            y=(Math.random()*contianer.clientHeight);
-            x=(Math.random()*contianer.clientWidth);
-            condition=((x==ele0.x || y==ele0.y));
-            while(condition){
-                y=(Math.random()*contianer.clientHeight);
-                x=(Math.random()*contianer.clientWidth);
-            }
-            console.log(i);
-            if(i>10 && i<20) {
-                ele.innerText="I said don't Click Me😒";
-            }
-            else if (i>20) {
-                ele.innerText="F*ck 😡 I said don't touch me";
-            } 
-            else{ 
-                ele.innerText="Don't Click Me🙄";
-            };
-            ele.style.position="Absolute";
-            ele.style.top=y+"px";
-            ele.style.right=x+"px";
-        }else{
-            if(i==1){
-                document.getElementsByClassName("reply")[0].innerText="Hello Pretty Girl🥵"
-            }else document.getElementsByClassName("reply")[0].innerText="Believe me ! you are Pretty !😘" ;
-            btn.style.display="None";
-        }
+        action(i,e);
         
     })
+    document.querySelector(".no").addEventListener("mouseover",(e)=>{
+        i++;
+        action(i,e);
+        
+    })
+}
+function action(i,e){
+    var contianer=document.getElementsByClassName("container")[0];
+    const ele=e.target;
+    const ele0=document.getElementsByClassName("btn")[0].getBoundingClientRect();
+    if(ele.classList.contains("no")){
+        var x,y;
+        y=(Math.random()*contianer.clientHeight);
+        x=(Math.random()*contianer.clientWidth);
+        condition=((x==ele0.x || y==ele0.y));
+        while(condition){
+            y=(Math.random()*contianer.clientHeight);
+            x=(Math.random()*contianer.clientWidth);
+        }
+        console.log(i);
+        if(i>10 && i<20) {
+            ele.innerText="I said don't touch Me😒";
+        }
+        else if (i>20) {
+            ele.innerText="F*ck 😡 I said don't touch me";
+        } 
+        else{ 
+            ele.innerText="Don't touch Me🙄";
+        };
+        ele.style.position="Absolute";
+        ele.style.top=y+"px";
+        ele.style.right=x+"px";
+    }else{
+        if(i==1){
+            document.getElementsByClassName("reply")[0].innerText="Hello Pretty Girl🥵"
+        }else document.getElementsByClassName("reply")[0].innerText="Believe me ! you are Pretty !😘" ;
+        document.querySelector(".btn-contain").style.display="None";
+    }
 }
 prank()
